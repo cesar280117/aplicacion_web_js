92bce87349ff8b42b19a8cf862f630e07feaee27
cesar280117 <cesar.cadesoft@gmail.com>
2023-01-26 14:57:25 -0600

    formulario

--- vue/carrito.js
+++ vue/carrito.js
@@ -1,7 +1,22 @@
 Vue.createApp({
     data() {
         return {
-
+            nombre: '',
+            precio: '',
+            cantidad: '',
+            url:'',
+            productos: []
         }
     },
+    methods: {
+        agregar() {
+            const producto = {
+                nombre: this.nombre,
+                precio: this.precio,
+                cantidad: this.cantidad,
+                url:this.url
+            }
+            this.productos.push(producto);
+        }
+    }
 }).mount('#app')
