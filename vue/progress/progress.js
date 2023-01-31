54cd1efd7298836a556c74f03bf704b2f7527cf5
cesar280117 <cesar.cadesoft@gmail.com>
2023-01-31 11:31:54 -0600

    si

--- vue/progress/progress.js
+++ vue/progress/progress.js
@@ -4,4 +4,15 @@ Vue.createApp({
             valor: 0
         }
     },
+    computed: {
+        color() {
+            if (this.valor > 0 && this.valor <= 40) {
+                return 'success';
+            } else if (this.valor > 41 && this.valor <= 80) {
+                return 'warning';
+            } else if (this.valor > 81 && this.valor <= 100) {
+                return 'danger';
+            }
+        }
+    }
 }).mount('#app')
