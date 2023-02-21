04ee7b37f4cc7f14c4b409c17340453e8452218d
cesar280117 <cesar.cadesoft@gmail.com>
2023-02-21 09:27:21 -0600

    pokedex

--- parcial2/api pokemon/app.js
+++ parcial2/api pokemon/app.js
@@ -1,23 +1,30 @@
 Vue.createApp({
     data() {
         return {
-            pokemon: null
+            pokemones: []
         }
+    },
+    mounted() {
+        this.getPokemon();
     },
     methods: {
         async getPokemon() {
+
             //GET     ----- Obtener recursos
             //POST    ----- para crear recursos
             //PUT     -----  Modificar recursos
             //DELETE  -----  Eliminar Recuersos
 
+            var i = 1;
+            while (i <= 200) {
+                const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + i, {
+                    method: 'GET'
+                });
+                const pokemon = await response.json();
+                this.pokemones.push(pokemon);
+                i++;
+            }
 
-            const random = Math.floor(Math.random() * 630);
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+random, {
-                method: 'GET'
-            });
-            const data = await response.json();
-            this.pokemon = data;
         }
     }
 }).mount('#app')
